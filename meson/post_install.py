d252f67877337ea3f17c5fcff401ab12c52af70a
Horacio Daniel Ros <horaciodrs@gmail.com>
2020-09-02 22:44:40 -0300

    Icon design and now TradeSim Support installation with Icons

--- meson/post_install.py
+++ meson/post_install.py
@@ -5,7 +5,13 @@ import subprocess
 
 install_prefix = os.environ['MESON_INSTALL_PREFIX']
 schemadir = os.path.join(install_prefix, 'share/glib-2.0/schemas')
+datadir = os.path.join(install_prefix, 'share')
+desktop_database_dir = os.path.join(datadir, 'applications')
 
 if not os.environ.get('DESTDIR'):
     print('Compiling the gsettings shcemas...')
     subprocess.call(['glib-compile-schemas', schemadir])
+    print('Updating desktop database…')
+    subprocess.call(['update-desktop-database', '-q', desktop_database_dir])
+    print('Updating icon cache…')
+    subprocess.call(['gtk-update-icon-cache', '-qtf', os.path.join(datadir, 'icons', 'hicolor')])
